0cae013569e8cb69ee179e508674599b7e587fd2
Sean Behan <inbox@seanbehan.com>
2014-04-19 17:42:49 -0400

    misc

--- webpager.rb
+++ webpager.rb
@@ -1,23 +1,23 @@
-require 'nokogiri' 
+require 'nokogiri'
 require 'open-uri'
- 
+
 # page = Webpager.new('http://seanbehan.com')
 # page.html
 # page.text
 # page.excerpt
 class Webpager
   def initialize(url)
-    @url = url  
+    @url = url
   end
-  
+
   def html
     @html ||= open(@url.strip).read
   end
-  
+
   def doc
     @doc ||= Nokogiri::HTML(html)
   end
-  
+
   def text
     all_tags     = /<\/?[^>]+>/i
     ref_tags     = /<(a|img)(.*)>/i
@@ -25,9 +25,9 @@ class Webpager
     style_tags   = /<style\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/style>/i
     iframe_tags  = /<iframe\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/iframe>/i
     comment_tags = /<!--\b[^<]*(?:(?!<\/script>)<[^<]*)*-->/i
-    
+
     regexp = Regexp.union(all_tags, script_tags, style_tags, iframe_tags, comment_tags)
-    
+
     body
     .gsub(ref_tags) { |tag| ((links = URI.extract(tag)).any? ? links.join(' ') : '') }
     .gsub(regexp, '')
@@ -36,26 +36,26 @@ class Webpager
     .reject(&:blank?)
     .join("\n")
   end
-  
+
   def body
     doc.xpath('//body').inner_html
   end
- 
+
   def excerptable?(text='')
     text.split('.').size >= 2 && text.size > 100
   end
- 
+
   def title
     html.match(/<title>(.*)<\/title>/) { $1 }
   end
-  
+
   def favicon
     # doc.xpath('//link/').select { |link| link.value =~ /favi/ }
   end
- 
+
   def excerpt
-    (doc.xpath('//p').map do |x| 
-      excerptable?(x.content) ? x.content : nil 
+    (doc.xpath('//p').map do |x|
+      excerptable?(x.content) ? x.content : nil
     end.compact.first||"").strip
   end
 end
